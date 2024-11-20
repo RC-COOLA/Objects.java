4f0c6c8e1273f93325cb358cdcf6a8a6a63c7536
RC-COOLA <a2jministries@gmail.com>
2024-11-20 21:27:54 +0000

    Main

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(120, 50.0, 4);
+        car.start();           
+        car.displayFuel();     
+        car.honk();            
+
+        Bicycle bicycle = new Bicycle(25, 0.0, true);
+        bicycle.start();       
+        bicycle.displayFuel(); 
+        bicycle.ringBell();    
+
+        car.setSpeed(150);
+        car.setFuel(40.0);
+        System.out.println("Updated Car Speed: " + car.getSpeed() + " MPH");
+        System.out.println("Updated Car Fuel: " + car.getFuel() + " liters");
+
+        bicycle.setHasBell(false);
+        bicycle.ringBell();
+    }
+}
