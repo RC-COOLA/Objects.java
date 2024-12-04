e1b37e56253efc04a88a8d38bdfc096d1a659dfe
RC-COOLA <a2jministries@gmail.com>
2024-12-03 18:39:42 -0600

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,24 +1,25 @@
 public class Car extends Vehicle {
     private int numberOfDoors;
-    public Car(int speed, double fuel, int numberOfDoors) {
+    public Car(int speed, float fuel, int numberOfDoors) {
         super(speed, fuel);
         this.numberOfDoors = numberOfDoors;
     }
-    public void start() {
-        System.out.println("The car has started. Vroom Vroom!");
+    public void start ()
+    {
+        System.out.println("The car has started.");
     }
 
     // Method for the honk
-    public void honk() {
+    public void honk () {
         System.out.println("Beep Beep!");
     }
 
-    // Getter for numberOfDoors is fixed
+    // Getter for numberOfDoors
     public int getNumberOfDoors() {
         return this.numberOfDoors;
     }
 
-    // Setter for numberOfDoors is set
+    // Setter for numberOfDoors
     public void setNumberOfDoors(int numberOfDoors) {
         this.numberOfDoors = numberOfDoors;
     }
